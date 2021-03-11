9ac7fd2c37d4e16e665f6d2d951098241563ab79
Diego Vester <vestergee3@gmail.com>
2021-03-11 17:48:59 -0600

    Update mod-logs.js

--- 42-Give-Role-Command/mod-logs.js
+++ 42-Give-Role-Command/mod-logs.js
@@ -1,1 +1,26 @@
+const roles = ['Moderator']
 
+module.exports = (client) => {
+  const channelId = '819718498749906954'
+
+  client.on('message', (message) => {
+    const { guild, content, member } = message
+
+    if (member.user.bot) {
+      return
+    }
+
+    const hasRole = member.roles.cache.find((role) => {
+      return roles.includes(role.name)
+    })
+
+    if (hasRole) {
+      const channel = guild.channels.cache.get(channelId)
+      if (channel) {
+        channel.send(`<@${member.id}> said this:
+      
+"${content}"`)
+      }
+    }
+  })
+}
